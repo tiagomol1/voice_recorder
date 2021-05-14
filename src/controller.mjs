945a8a998ad4898555bd69c15183a485d614020c
Tiago Murilo <tiago.murilo08@gmail.com>
2021-05-14 14:36:51 -0300

    voice recorder, complete

---
+++ src/controller.mjs
@@ -0,0 +1,34 @@
+export default class Controller{
+
+    constructor({ view, media, recorder }){
+        this.view = view
+        this.media = media
+        this.recorder = recorder
+    }
+
+    static initialize(dependecies){
+        const instance = new Controller(dependecies)
+        return instance._init()
+    }
+
+
+    _init(){
+        this.view.configureStartRecordingButton(this.onStartRecording.bind(this))
+        this.view.configureStopRecordingButton(this.onStopRecording.bind(this))
+    }
+
+    async onStartRecording(){
+        const audioStream = await this.media.getAudio()
+        this.recorder.startRecording(audioStream)
+    }
+
+    async onStopRecording(){
+        this.recorder.stopRecording()
+        
+        setTimeout(() => {
+            const audioURL = this.recorder.getRecordingURL()
+            this.view.playAudio(audioURL)
+        })
+    }
+
+}
---
+++ src/util/media.mjs
@@ -0,0 +1,9 @@
+export default class Media{
+
+    async getAudio(){
+        return navigator.mediaDevices.getUserMedia({
+            audio: true
+        })
+    }
+
+}
